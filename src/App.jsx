a94a7394a411df1ed4b16f378d5b68382e4c1daf
Jose Pinto <alipinko123@gmail.com>
2023-05-11 12:07:39 -0500

    the majority of the project has been completed

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import "./App.css";
+import * as Icons from "react-icons/all";
+import { AiOutlineReload } from "react-icons/ai";
+import cardData from "./data/cards";
+import { useState } from "react";
+
+function App() {
+  const [cardList, setCardList] = useState(
+    cardData.sort(() => Math.random() - 0.5)
+  );
+  const [prevIndexCard, setPrevIndexCard] = useState(-1);
+
+  const handleClick = (index) => {
+    cardList[index].status = "selected";
+    setCardList([...cardList]);
+    if (prevIndexCard === -1) {
+      setPrevIndexCard(index);
+    } else {
+      validateCards(index);
+      /*  setPrevIndexCard(-1); */
+    }
+  };
+
+  const validateCards = (newIndexCard) => {
+    setTimeout(() => {
+      if (
+        cardList[prevIndexCard].icon === cardList[newIndexCard].icon &&
+        cardList[prevIndexCard].id !== cardList[newIndexCard].id
+      ) {
+        cardList[prevIndexCard].status = "up";
+        cardList[newIndexCard].status = "up";
+        setCardList([...cardList]);
+        setPrevIndexCard(-1);
+      } else {
+        cardList[prevIndexCard].status = "down";
+        cardList[newIndexCard].status = "down";
+        setCardList([...cardList]);
+        setPrevIndexCard(-1);
+      }
+    }, 500);
+  };
+  return (
+    <div className="main-container">
+      <h1 className="title-main">Memory Cards</h1>
+      <div className="cards-container">
+        {cardList.map((card, index) => {
+          const Icon = Icons[card.icon];
+          return (
+            <div
+              key={card.id}
+              className={`${card.status} card`}
+              onClick={() => handleClick(index)}
+            >
+              {card.status !== "down" && <Icon className="icons__cards" />}
+            </div>
+          );
+        })}
+      </div>
+      <div className="container-reload">
+        <AiOutlineReload className="reload" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/cards.js
@@ -0,0 +1,83 @@
+const cardData = [
+  {
+    id: 1,
+    icon: "BsFillHouseDoorFill",
+    status: "down",
+  },
+  {
+    id: 2,
+    icon: "BsFillHouseDoorFill",
+    status: "down",
+  },
+  {
+    id: 3,
+    icon: "AiFillAlipayCircle",
+    status: "down",
+  },
+  {
+    id: 4,
+    icon: "AiFillAlipayCircle",
+    status: "down",
+  },
+  {
+    id: 5,
+    icon: "AiFillApple",
+    status: "down",
+  },
+  {
+    id: 6,
+    icon: "AiFillApple",
+    status: "down",
+  },
+  {
+    id: 7,
+    icon: "AiFillCreditCard",
+    status: "down",
+  },
+  {
+    id: 8,
+    icon: "AiFillCreditCard",
+    status: "down",
+  },
+  {
+    id: 9,
+    icon: "FaArtstation",
+    status: "down",
+  },
+  {
+    id: 10,
+    icon: "FaArtstation",
+    status: "down",
+  },
+  {
+    id: 11,
+    icon: "FaBattleNet",
+    status: "down",
+  },
+  {
+    id: 12,
+    icon: "FaBattleNet",
+    status: "down",
+  },
+  {
+    id: 13,
+    icon: "FaBtc",
+    status: "down",
+  },
+  {
+    id: 14,
+    icon: "FaBtc",
+    status: "down",
+  },
+  {
+    id: 15,
+    icon: "FaEnvira",
+    status: "down",
+  },
+  {
+    id: 16,
+    icon: "FaEnvira",
+    status: "down",
+  },
+];
+export default cardData;
